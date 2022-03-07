bfa104d68bf5f1cdf0dcdd7af1d05d0b0b2e1a5c
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-03-07 11:23:39 -0300

    creating user dynamically

--- src/interface/controller/user_controller.go
+++ src/interface/controller/user_controller.go
@@ -3,7 +3,6 @@ package controller
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -23,50 +22,77 @@ import (
 // "postgres://postgres@localhost/testdb?sslmode=disable"
 
 type User struct {
-	ID        int    `json:"user_id" db:"user_id"`
+	ID int `json:"user_id" db:"user_id"`
+
+	// I put Name, because if I put UserName when going to use
+	// would have to call user.UserName and I don't like that
+	// user.Name is already implied
+	//
+	// Coloquei Name, porque se eu colocasse UserName quando fosse usar
+	// iria ter que chamar user.UserName e eu não gosto disso
+	//  user.Name já fica subentendido
 	Name      string `json:"username" db:"username"`
+	FullName  string `json:"fullname" db:"fullname"`
 	Email     string `json:"email" db:"email"`
-	Password  string `json:"password" db:"passwd"`
+	Password  string `json:"passwd" db:"passwd"`
 	CreatedAt string `json:"created_at" db:"created_at"`
 	UpdatedAt string `json:"updated_at" db:"updated_at"`
 }
 
+// There is an error here in returning user data
+// it doesn't show the ID correctly, nor the Creation Date
+// even though the user was created
+// if you go in the route that shows all users you will see that
+// he was raised
+// Don't worry
+// this could be changed in new feature
+//
+//  "user_id": 0,
+//  "created_at": "",
+//  "updated_at": ""
+//
+// Existe um erro aqui no retorno dos dados do usuario
+// ele não mostra o ID corretamente, nem a Data de criação
+// mesmo que o user foi criado
+// se você for na rota que mostra todos os usuarios você vai ver que
+// ele foi criado
+// Não se preocupe
+// Isso poderia ser mudado em uma nova feature
+//
 func (user *User) CreateUser() http.HandlerFunc {
 	return func(writer http.ResponseWriter, req *http.Request) {
 		writer.Header().Set("Content-Type", "application/json")
 
-		body, _ := ioutil.ReadAll(req.Body)
+		json.NewDecoder(req.Body).Decode(user)
 
-		fmt.Println(string(body))
-
-		row, err := config.Insert(
+		_, err := config.Insert(
 			`INSERT INTO user_schema (
 				username, fullname, email, passwd
 			)
 			VALUES ($1, $2, $3, $4)
 			RETURNING *`,
-			// []string{"hamilton", "José Hamilton", "jose@hamilton.com", "123"},
-			"hamilton", "José Hamilton", "jose@hamilton.com", "123",
+			user.Name, user.FullName, user.Email, user.Password,
 		)
 		if err != nil {
 			writer.WriteHeader(http.StatusInternalServerError)
 
 			json.NewEncoder(writer).Encode(map[string]string{
-				"message": fmt.Sprintf("erro in Insert: %v", err),
+				"message": fmt.Sprintf("erro in Insert in create user: %v", err),
 			})
 
 			return
 		}
 
-		// 	namedSQL := `INSERT INTO "clients" ("name","address") VALUES (:name,:address)`
+		// I'm putting the "", to overwrite password,
+		// and don't display it to the end user
+		// please do not use this in frontend application
+		//
+		// Eu estou colocando o "", para sobrescrever o password,
+		// e não exibir para ao usuário final
+		// por favor não use isso no frontend
+		user.Password = ""
 
-		// _, err = db.NamedExec(namedSQL,
-		// 	client{
-		// 		Name:    "Cyberdyne Systems",
-		// 		Address: "2144 Kramer St",
-		// 	})
-
-		json.NewEncoder(writer).Encode(row)
+		json.NewEncoder(writer).Encode(user)
 	}
 
 }
@@ -81,7 +107,7 @@ func (user *User) ListAllUsers() http.HandlerFunc {
 			writer.WriteHeader(http.StatusInternalServerError)
 
 			json.NewEncoder(writer).Encode(map[string]string{
-				"message": fmt.Sprintf("erro in Select query: %v", err),
+				"message": fmt.Sprintf("erro in select query, in select all users: %v", err),
 			})
 
 			return
@@ -91,7 +117,7 @@ func (user *User) ListAllUsers() http.HandlerFunc {
 			user := User{}
 
 			err = rows.Scan(
-				&user.ID, &user.Name, &user.Email,
+				&user.ID, &user.Name, &user.FullName, &user.Email,
 				&user.Password, &user.CreatedAt, &user.UpdatedAt,
 			)
 			if err != nil {
@@ -109,7 +135,7 @@ func (user *User) ListAllUsers() http.HandlerFunc {
 			// please do not use this in frontend application
 			//
 			// Eu estou colocando o "", para sobrescrever o password,
-			// e não exibir par ao usuário final
+			// e não exibir para ao usuário final
 			// por favor não use isso no frontend
 			user.Password = ""
 
@@ -140,7 +166,7 @@ func (user *User) ListAllUsers() http.HandlerFunc {
 func (user *User) ListUser() http.HandlerFunc {
 	return func(writer http.ResponseWriter, req *http.Request) {
 		params := mux.Vars(req)
-		query := fmt.Sprintf("SELECT * FROM user_schema WHERE user_id=%v", params["user_id"])
+		query := fmt.Sprintf("SELECT * FROM user_schema WHERE user_id=%v", params["id"])
 
 		writer.Header().Set("Content-type", "application/json")
 
@@ -152,7 +178,7 @@ func (user *User) ListUser() http.HandlerFunc {
 			writer.WriteHeader(http.StatusInternalServerError)
 
 			json.NewEncoder(writer).Encode(map[string]string{
-				"message": fmt.Sprintf("erro in Select query: %v", err),
+				"message": fmt.Sprintf("erro in select query, in select unique user: %v", err),
 			})
 
 			return
@@ -160,7 +186,7 @@ func (user *User) ListUser() http.HandlerFunc {
 
 		for row.Next() {
 			err = row.Scan(
-				&user.ID, &user.Name, &user.Email,
+				&user.ID, &user.Name, &user.FullName, &user.Email,
 				&user.Password, &user.CreatedAt, &user.UpdatedAt,
 			)
 			if err != nil {
--- src/interface/router/user_router.go
+++ src/interface/router/user_router.go
@@ -26,5 +26,5 @@ func (server *Server) User() {
 	server.HandleFunc("/api/v1/user/{id:[0-9]+}", user.ListUser()).Methods("GET")
 	server.HandleFunc("/api/v1/user/{id:[0-9]+}", user.UpdateUser()).Methods("PUT")
 
-	// server.HandleFunc("/api/v1/user/{id}", user.CreateUser()).Methods("DELETE")
+	server.HandleFunc("/api/v1/user/{id:[0-9]+}", user.CreateUser()).Methods("DELETE")
 }
